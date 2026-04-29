80b93c52ef91960599a0233377b9018d59d9848b
zurakov <zafarurakov@outlook.com>
2026-04-29 21:36:50 +0800

    CODE RUB: Fix Comments

--- Program.cs
+++ Program.cs
@@ -10,20 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// SQLite Database Configuration
 builder.Services.AddDbContext<SignatureRequestDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Brokers
 builder.Services.AddTransient<IDocuSignBroker, DocuSignBroker>();
 builder.Services.AddScoped<IStorageBroker, StorageBroker>();
-
-// Foundation Services
 builder.Services.AddTransient<IDocuSignService, DocuSignService>();
 builder.Services.AddTransient<ISignatureRequestService, SignatureRequestService>();
 builder.Services.AddTransient<IQuickBooksService, QuickBooksService>();
-
-// Enable CORS if needed (though UI is now local)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -31,8 +25,6 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
-
-// Auto-Apply Migrations at Startup (Simplified for POC)
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<SignatureRequestDbContext>();
